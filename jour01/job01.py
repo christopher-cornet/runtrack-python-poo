4e4b9a4ed829fe75d2826f4bf238d977b975d8de
christopher-cornet <115154379+christopher-cornet@users.noreply.github.com>
2023-02-27 10:54:49 +0100

    Add files via upload

---
+++ jour01/job01.py
@@ -0,0 +1,6 @@
+class Operation:
+    def __init__(self):
+        self.nombre1 = 5
+        self.nombre2 = 10
+
+print(Operation())
---
+++ jour01/job02.py
@@ -0,0 +1,8 @@
+class Operation:
+    def __init__(self, nombre1, nombre2):
+        self.nombre1 = nombre1
+        self.nombre2 = nombre2
+        print('Le nombre1 est', self.nombre1)
+        print('Le nombre2 est', self.nombre2)
+         
+Operation(5, 10)
