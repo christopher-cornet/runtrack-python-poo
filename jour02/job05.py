7f6d3c4e1793aba1058cab0d4a32be83a5312cb5
christopher-cornet <115154379+christopher-cornet@users.noreply.github.com>
2023-03-01 09:29:22 +0100

    Add files via upload

--- jour02/job05.py
+++ jour02/job05.py
@@ -17,7 +17,53 @@ class Voiture:
 
     def __verifier_plein(self):
         return self.__reservoir
+    
+    # Get
+    def getmarque(self):
+        return self.__marque
+    
+    def getmodele(self):
+        return self.__modèle
+    
+    def getannee(self):
+        return self.__année
+    
+    def getkilometrage(self):
+        return self.__kilométrage
+    
+    # Set
+    def setmarque(self, marque):
+        self.__marque = marque
 
-car = Voiture('Tesla', 'Model X', 2016, 40000, 50)
+    def setmodele(self, modele):
+        self.__modèle = modele
+    
+    def setannee(self, annee):
+        self.__année = annee
+    
+    def setkilometrage(self, km):
+        self.__kilométrage = km
+
+car = Voiture('Tesla', 'Model X', 2016, 43874, 50)
 
 car.demarrer()
+
+# Get
+print(car.getmarque())
+print(car.getmodele())
+print(car.getannee())
+print(car.getkilometrage())
+
+# Set
+car.setmarque('Koenigsegg')
+car.setmodele('Gemera')
+car.setannee(2021)
+car.setkilometrage(55348)
+
+print('Modification:')
+
+# Modif
+print(car.getmarque())
+print(car.getmodele())
+print(car.getannee())
+print(car.getkilometrage())
