22e711d4366ce2d4bf06d63ce67b0f10a40fa35c
christopher-cornet <115154379+christopher-cornet@users.noreply.github.com>
2023-03-01 22:50:25 +0100

    Add files via upload

--- jour03/job03.py
+++ jour03/job03.py
@@ -1,40 +1,43 @@
 class Tache:
-    def __init__(self, titre, description, statut):
+    def __init__(self, titre, description, statut='A faire'):
         self.titre = titre
         self.description = description
         self.statut = statut
 
 class ListeDeTaches:
     def __init__(self):
-        self.taches = []
+        self.liste_taches = []
 
     # ajouter une tache
     def ajouterTache(self, titre):
-        self.taches.append(titre)
-        print(self.taches)
+        self.liste_taches.append(titre)
     
     # supprimer une tache
     def supprimerTache(self, titre):
-        self.taches.remove(titre)
-        print(self.taches)
+        self.liste_taches.remove(titre)
 
     # signaler que la tache est faite.
     def marquerCommeFinie(self):
-        self.taches.append('Finie')
+        # self.liste_taches.append('Finie')
+        pass
     
     # retourner une liste de toutes les taches.
     def afficherListe(self):
-        print(self.taches)
+        for tache in self.liste_taches:
+            print(f'{tache.titre} - {tache.description} - {tache.statut}')
 
     # filtrer les taches par rapport à un statut et retourne
     # cette liste.
     def filterListe(self):
         pass
 
-tache1 = Tache('travailler', 'coder', 'à faire')
+tache1 = Tache('coder', 'apprendre la POO')
+tache2 = Tache('réparer voiture', 'changer la batterie')
 
-tache2 = ListeDeTaches()
-tache2.ajouterTache('coder')
-tache2.ajouterTache('travailler')
-tache2.ajouterTache('travailler')
-tache2.supprimerTache('coder')
+taches = ListeDeTaches()
+
+taches.ajouterTache(tache1)
+taches.ajouterTache(tache2)
+
+print("Liste des taches:")
+taches.afficherListe()
