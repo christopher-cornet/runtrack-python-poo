a01522dc9b674934095193df18271609068356ed
christopher-cornet <115154379+christopher-cornet@users.noreply.github.com>
2023-03-04 10:12:36 +0100

    Update job03.py

--- jour05/job03.py
+++ jour05/job03.py
@@ -1,7 +1,7 @@
 def returnLength(string1):
-    if string1 == '': # Si la chaine de caractère ne contient pas de caractère
+    if string1 == '': # Si la chaine de caractère ne contient pas de caractère return 0
         return 0
     else:
-        return 1 + returnLength(string1[1:]) # Return longueur de la string
+        return 1 + returnLength(string1[1:]) # Sinon Return 1 et longueur de la string -1
 
 print(returnLength('Echecs'))
